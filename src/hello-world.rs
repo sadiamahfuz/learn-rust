ebcb90176c496c9949029387ab3cccf421f2752d
Sadia Mahfuz <sadia.mahfuz@gmail.com>
2022-12-28 15:58:49 +0000

    intro to variables

--- src/hello-world.rs
+++ src/hello-world.rs
@@ -1,5 +1,8 @@
 fn main() {
     println!("Hello, world!");
 
-    println!("My name is {} and I was born in {}", "Sadia", 1990);
+    let name = "Sadia";
+    let year_of_birth = 1990;
+
+    println!("My name is {} and I was born in {}", name, year_of_birth);
 }
