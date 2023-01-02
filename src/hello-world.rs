100f5619a148b5827f2ceb637cb68e178621ca37
Sadia Mahfuz <sadia.mahfuz@gmail.com>
2023-01-02 10:41:28 +0000

    add types to variables

--- src/hello-world.rs
+++ src/hello-world.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
 
-    let name = "Sadia";
-    let year_of_birth = 1990;
+    let name: &str = "Sadia";
+    let year_of_birth: u32 = 1990;
 
     println!("My name is {} and I was born in {}", name, year_of_birth);
 }
